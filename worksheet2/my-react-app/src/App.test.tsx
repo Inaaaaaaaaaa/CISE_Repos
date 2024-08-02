403d934cba9ca145c9b584e85e323835d26c6fee
Latte123456 <inayumi7@gmail.com>
2024-08-02 13:40:32 +1200

    New files - working

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,28 +1,8 @@
-import React, { Component } from "react";
-interface State {
-count: number;
-}
-class App extends Component<{}, State> {
-constructor(props: {}) {
-super(props);
-this.state = {
-count: 0,
-};
-}
-makeIncrementer = (amount: number) => () =>
-this.setState((prevState: State) => ({
-count: prevState.count + amount,
-}));
-increment = this.makeIncrementer(1);
-render() {
-return (
-<div>
-<p>Count: {this.state.count}</p>
-<button className="increment" onClick={this.increment}>
-Increment count
-</button>
-</div>
-);
-}
-}
-export default App;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+it('renders without crashing', () => {
+const div = document.createElement('div');
+ReactDOM.render(<App />, div);
+ReactDOM.unmountComponentAtNode(div);
+});
